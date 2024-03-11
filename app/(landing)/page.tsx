0686351161e08d027e6dd28d0dd2d516fce025c5
Sritharan Thiyagarajah <sk.sritharan75@gmail.com>
2024-03-11 18:40:00 +0530

    Authentication setup

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+const LandingPage = () => {
+  return (
+    <div>
+      LandingPage
+      <div>
+        <Button>
+          <a href="/sign-in">Login</a>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
